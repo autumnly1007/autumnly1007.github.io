328b867c4ffe3a7e089bb03d1cab4a812bb73ba6
autumnly1007 <autumnly1007@gmail.com>
2022-04-03 12:39:26 +0900

    Update index.js

--- src/components/post-header/index.js
+++ src/components/post-header/index.js
@@ -18,9 +18,9 @@ function PostHeader({ post, viewCount }) {
 
       <h1 className="title">{post.title}</h1>
       <div className="info">
-        <div className="author">
+        {/* <div className="author">
           posted by <strong>{post.author}</strong>,
-        </div>{' '}
+        </div>{' '} */}
         {post.date}
         {viewCount && ` · ${viewCount} views`}
       </div>
